1c971dd5d182cda3ae083fb1c148c772c56bcd99
Sourbiebie <sourbiebie@gmail.com>
2021-11-27 18:06:38 +0800

    roll back to the initial version
    Remove testing code

--- back-end/www/controllers/root.py
+++ back-end/www/controllers/root.py
@@ -1,8 +1,6 @@
 """The controller for https://[PATH]/"""
 
 from models.model import User
-from models.model_operations import user_operations
-from models.model_operations import location_operations
 from flask import Blueprint
 
 
@@ -11,13 +9,5 @@ bp = Blueprint("root", __name__)
 
 @bp.route("/")
 def hello_world():
-    #print(User.query.all())
-    users = user_operations.get_all_users()
-    location_count = location_operations.get_location_count()
-    #location_count = 10
-    #print("Users: ", len(users))
-    #return_string = "Hello, world! users:{} ".format(len(users))
-    return_string = "Hello, world! users:{} locations:{}".format(len(users), location_count)
-    #return_string = "Hello, world! users:{} locations:".format(0, location_count)
-    #return "Hello, World!"
-    return return_string
+    print(User.query.all())
+    return "Hello, World!"
