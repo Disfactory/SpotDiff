9246478918f17667b1544a26d438202d95a0e974
Sourbiebie <sourbiebie@gmail.com>
2021-11-27 17:54:10 +0800

    Clean code and update document
     Add documents and remove redundant code

--- back-end/www/controllers/location_controller.py
+++ back-end/www/controllers/location_controller.py
@@ -10,13 +10,10 @@ from util.util import handle_invalid_usage
 from util.util import encode_jwt
 from util.util import try_wrap_response
 from config.config import config
-#from models.model_operations.user_operations import get_user_by_client_id
-#from models.model_operations.user_operations import create_user
 from models.model_operations.location_operations import get_locations
 from models.schema import locations_schema
 
 bp = Blueprint("location_controller", __name__)
-
 
 @bp.route("/", methods=["GET"])
 def location():
@@ -30,11 +27,14 @@ def location():
     ----------
     client_id : str
         The client ID string provided by the front-end client.
+    size : int
+        Total number of locations to be returned.s
+    gold_standard_size : int    
+        Within size, the number of locations which includes gold standard answers    
 
     Returns
     -------
-    user_token : str
-        The encoded JWT that stores user information.
+        The encoded JWT that stores location information.
     """    
     if request.method == "GET":
         request_json = request.get_json()
@@ -64,72 +64,3 @@ def location():
 def try_get_locations(size, gold_standard_size):
     data = get_locations(size, gold_standard_size)
     return jsonify({"data": locations_schema.dump(data)})
-
-
-
-
-
-
-
-
-
-
-
-    client_id = None
-    request_json = request.get_json()
-    if request_json is not None:
-        if "client_id" in request_json:
-            client_id = request_json["client_id"]
-
-    # Get user id by client id, and issued an user jwt
-    if client_id is None:
-        e = InvalidUsage("Must have 'client_id'.", status_code=400)
-        return handle_invalid_usage(e)
-    else:
-        user_token = get_user_token_by_client_id(client_id)
-        if user_token is None:
-            # This happens when the user is banned
-            e = InvalidUsage("Permission denied.", status_code=403)
-            return handle_invalid_usage(e)
-        else:
-            return_json = {"user_token": user_token}
-            return jsonify(return_json)
-
-
-def get_user_token_by_client_id(client_id):
-    """
-    Get the encoded user token by using client id.
-
-    Parameters
-    ----------
-    client_id : str
-        The ID provided by the front-end client.
-
-    Returns
-    -------
-    user_token : str
-        The JWT (JSON Web Token) of the corresponding user.
-    """
-    user = get_user_by_client_id(client_id)
-    if user is None:
-        user = create_user(client_id) # create a new user if not found
-    user_id = user.id
-    client_type = user.client_type
-    if client_type == -1:
-        return None # a banned user does not get the token
-    else:
-        user_token = encode_user_jwt(user_id=user_id, client_type=client_type)
-        return user_token
-
-
-def encode_user_jwt(**kwargs):
-    """Encode user JWT (JSON Web Token)."""
-    t = kwargs["iat"] if "iat" in kwargs else round(time.time())
-    payload = {}
-    payload["iat"] = t
-    payload["jti"] = uuid.uuid4().hex
-    payload["iss"] = "[CHANGE_THIS_TO_YOUR_ROOT_URL]"
-    payload["exp"] = t + 3600 # the token will expire after one hour
-    for k in kwargs:
-        payload[k] = kwargs[k]
-    return encode_jwt(payload, config.JWT_PRIVATE_KEY)
--- back-end/www/controllers/root.py
+++ back-end/www/controllers/root.py
@@ -2,6 +2,7 @@
 
 from models.model import User
 from models.model_operations import user_operations
+from models.model_operations import location_operations
 from flask import Blueprint
 
 
@@ -12,7 +13,11 @@ bp = Blueprint("root", __name__)
 def hello_world():
     #print(User.query.all())
     users = user_operations.get_all_users()
-    print("Users: ", len(users))
-    return_string = "Hello, world! {}".format(len(users))
+    location_count = location_operations.get_location_count()
+    #location_count = 10
+    #print("Users: ", len(users))
+    #return_string = "Hello, world! users:{} ".format(len(users))
+    return_string = "Hello, world! users:{} locations:{}".format(len(users), location_count)
+    #return_string = "Hello, world! users:{} locations:".format(0, location_count)
     #return "Hello, World!"
     return return_string
