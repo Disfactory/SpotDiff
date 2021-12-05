ef197f0faaee55e82a8bd16931857fc19429d5a5
YAlgorithm <legenddolphin.public@gmail.com>
2021-12-05 14:13:35 +0100

    Improve documentation and format of the answer_operations.py file.

--- back-end/www/models/model_operations/answer_operations.py
+++ back-end/www/models/model_operations/answer_operations.py
@@ -4,56 +4,71 @@ from models.model import db
 from models.model import Answer
 
 
-def create_answer(user_id, location_id, year_old, year_new, source_url_root, land_usage, expansion, is_gold_standard, bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat, bbox_bottom_right_lng, zoom_level):
+def create_answer(user_id, location_id, year_old, year_new,
+        source_url_root, land_usage, expansion, is_gold_standard,
+        bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat,
+        bbox_bottom_right_lng, zoom_level):
     """
     Create an answer reported by an user with a location.
 
     Parameters
     ----------
     user_id : int
-        Foreign key to User table
+        Foreign key to the user table.
     location_id : int
-        Foreign key to Location table
+        Foreign key to the location table.
     year_old: int
-        year Marks which year the satellite photo was taken from our geo sources. 
+        The year where the satellite photo was taken from our geo sources.
     year_new: int
-        A newer photo to be compared with the one taken in year_old
+        A newer photo to be compared with the one taken in year_old.
     source_url_root : str
         URL to store the location on the map.
     land_usage : int
-        User's answer of judging a construction is built. 0: construction, 1: unknown, 2: farm
+        User's answer of judging if the land is a farm or has buildings.
+        (check the answer table in model.py for the meaning of the values)
     expansion : int
-        User's answer of judging the construction is expanded. 0: unknown, 1: nope, 2:yes
-    is_gold_standard : Boolean
+        User's answer of judging the construction is expanded.
+        (check the answer table in model.py for the meaning of the values)
+    is_gold_standard : bool
         If it's a golden standard answer provided by admnin.
-    bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat, bbox_bottom_right_lng : float
-        The coordinates for the 2 points forming the inner boundbox for displaying the focus.        
+    bbox_left_top_lat : float
+        The latitude of the top-left corner of the bounding box for displaying the focus.
+    bbox_left_top_lng : float
+        The longitude of the top-left corner of the bounding box for displaying the focus.
+    bbox_bottom_right_lat : float
+        The latitude of the bottom-right corner of the bounding box for displaying the focus.
+    bbox_bottom_right_lng : float
+        The longitude of the bottom-right corner of the bounding box for displaying the focus.
     zoom_level : int
-        The zoom level for displaying.        
+        The zoom level for displaying the location.
+
     Returns
     -------
     answer : Answer
-        The newly created answer.  
+        The newly created answer.
 
     Raises
     ------
     exception : Exception
-        Type checking for land_usage, expansion, year_old, year_new to be integers, is_gold_standard to be boolean.
+        Type checking for land_usage, expansion, year_old, year_new, and is_gold_standard.
     """
-    if(not isinstance(land_usage, int)):
+    if not isinstance(land_usage, int):
         raise Exception("The land_usage shall be an integer")
-    if(not isinstance(expansion, int)):
+    if not isinstance(expansion, int):
         raise Exception("The expansion shall be an integer")
-    if(not isinstance(is_gold_standard, bool)):
+    if not isinstance(is_gold_standard, bool):
         raise Exception("The is_gold_standard shall be a bool")
-    if(not isinstance(year_old, int)):
+    if not isinstance(year_old, int):
         raise Exception("The photo year shall be an integer")
-    if(not isinstance(year_new, int)):
+    if not isinstance(year_new, int):
         raise Exception("The photo year shall be an integer")
 
-    answer = Answer(user_id=user_id, location_id=location_id, year_old=year_old, year_new=year_new, source_url_root=source_url_root, land_usage=land_usage, expansion=expansion,
-                    is_gold_standard=is_gold_standard, bbox_left_top_lat=bbox_left_top_lat, bbox_left_top_lng=bbox_left_top_lng,
-                    bbox_bottom_right_lat=bbox_bottom_right_lat, bbox_bottom_right_lng=bbox_bottom_right_lng, zoom_level=zoom_level)
+    answer = Answer(user_id=user_id, location_id=location_id, year_old=year_old,
+            year_new=year_new, source_url_root=source_url_root, land_usage=land_usage,
+            expansion=expansion, is_gold_standard=is_gold_standard,
+            bbox_left_top_lat=bbox_left_top_lat, bbox_left_top_lng=bbox_left_top_lng,
+            box_bottom_right_lat=bbox_bottom_right_lat,
+            bbox_bottom_right_lng=bbox_bottom_right_lng, zoom_level=zoom_level)
 
     db.session.add(answer)
     db.session.commit()
@@ -61,46 +76,61 @@ def create_answer(user_id, location_id, year_old, year_new, source_url_root, lan
     return answer
 
 
-def create_answer_with_result(user_id, location_id, year_old, year_new, source_url_root, land_usage, expansion, is_gold_standard, bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat, bbox_bottom_right_lng, zoom_level):
+def create_answer_with_result(user_id, location_id, year_old, year_new,
+        source_url_root, land_usage, expansion, is_gold_standard,
+        bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat,
+        bbox_bottom_right_lng, zoom_level):
     """
     Create an answer and return if it passed or not by checking the gold standards.
 
     Parameters
     ----------
     user_id : int
-        Foreign key to User table
+        Foreign key to the user table.
     location_id : int
-        Foreign key to Location table
+        Foreign key to the location table.
     year_old: int
-        year Marks which year the satellite photo was taken from our geo sources. 
+        The year where the satellite photo was taken from our geo sources.
     year_new: int
-        A newer photo to be compared with the one taken in year_old
+        A newer photo to be compared with the one taken in year_old.
     source_url_root : str
         URL to store the location on the map.
     land_usage : int
-        User's answer of judging a construction is built. 0: construction, 1: unknown, 2: farm
+        User's answer of judging if the land is a farm or has buildings.
+        (check the answer table in model.py for the meaning of the values)
     expansion : int
-        User's answer of judging the construction is expanded. 0: unknown, 1: nope, 2:yes
-    is_gold_standard : Boolean
+        User's answer of judging the construction is expanded.
+        (check the answer table in model.py for the meaning of the values)
+    is_gold_standard : bool
         If it's a golden standard answer provided by admnin.
-    bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat, bbox_bottom_right_lng : float
-        The coordinates for the 2 points forming the inner boundbox for displaying the focus.        
+    bbox_left_top_lat : float
+        The latitude of the top-left corner of the bounding box for displaying the focus.
+    bbox_left_top_lng : float
+        The longitude of the top-left corner of the bounding box for displaying the focus.
+    bbox_bottom_right_lat : float
+        The latitude of the bottom-right corner of the bounding box for displaying the focus.
+    bbox_bottom_right_lng : float
+        The longitude of the bottom-right corner of the bounding box for displaying the focus.
     zoom_level : int
-        The zoom level for displaying.        
+        The zoom level for displaying the location.
+
     Returns
     -------
-    compare_result : integer
-        Compare with the gold answer. 
-        0: Pass
-        1: Fail
-        -1: Gold doesn't exist
+    compare_result : int
+        Compare with the gold answer.
+        0 means Pass
+        1 means Fail
+        -1 means that the gold standard does not exist
 
     Raises
     ------
     exception : Exception
-        Type checking for land_usage, expansion, year_old, year_new to be integers, is_gold_standard to be boolean.
+        Type checking for land_usage, expansion, year_old, year_new, and is_gold_standard.
     """
-    new_answer = create_answer(user_id, location_id, year_old, year_new, source_url_root, land_usage, expansion, is_gold_standard, bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat, bbox_bottom_right_lng, zoom_level)
+    new_answer = create_answer(user_id, location_id, year_old, year_new,
+            source_url_root, land_usage, expansion, is_gold_standard,
+            bbox_left_top_lat, bbox_left_top_lng, bbox_bottom_right_lat,
+            bbox_bottom_right_lng, zoom_level)
 
     # Bypass the gold standard itself
     if(is_gold_standard):
@@ -108,21 +138,22 @@ def create_answer_with_result(user_id, location_id, year_old, year_new, source_u
 
     # Get the gold answer and compare result
     compare_result = 0
-    gold_answer = Answer.query.filter_by(is_gold_standard=True, location_id=location_id, year_old=year_old, year_new=year_new).first()
+    gold_answer = Answer.query.filter_by(is_gold_standard=True,
+            location_id=location_id, year_old=year_old, year_new=year_new).first()
     if(gold_answer != None):
         if ((gold_answer.land_usage == land_usage) and (gold_answer.expansion == expansion)):
             compare_result = 0
         else:
-            compare_result = 1    
+            compare_result = 1
     else:
         compare_result = -1
 
     return compare_result
-    
+
 
 def get_answer_count():
     """
-    Get total number of answers exclusive of gold answers
+    Get total number of answers, excluding gold answers.
     """
     answer_count = Answer.query.filter_by(is_gold_standard=False).count()
     return answer_count
@@ -140,7 +171,7 @@ def get_answer_by_id(answer_id):
     Returns
     -------
     answer : Answer
-        The retrieved answer object.    
+        The retrieved answer object.
     """
     answer = Answer.query.filter_by(id=answer_id).first()
     return answer
@@ -158,7 +189,7 @@ def get_answers_by_user(user_id):
     Returns
     -------
     answers : Answer
-        The list of answers reported by the specified user.    
+        The list of answers reported by the specified user.
     """
     answers = Answer.query.filter_by(user_id=user_id).all()
     return answers
@@ -176,7 +207,7 @@ def get_answers_by_location(location_id):
     Returns
     -------
     answers : Answer
-        The list of answers reported with the specified location.    
+        The list of answers reported with the specified location.
     """
     answers = Answer.query.filter_by(location_id=location_id).all()
     return answers
@@ -190,15 +221,15 @@ def get_answers_by_user_and_location(user_id, location_id):
     ----------
     user_id : int
         ID of the user.
-    location_id : int    
+    location_id : int
         ID of the location.
     Returns
     -------
     answers : Answer
-        The list of answers reported by the specified user and location.    
+        The list of answers reported by the specified user and location.
     """
     answers = Answer.query.filter_by(user_id=user_id, location_id=location_id).all()
-    
+
     return answers
 
 
@@ -213,7 +244,7 @@ def get_gold_answer_by_location(location_id):
     Returns
     -------
     answers : Answer
-        The gold answer.   
+        The gold answer.
     """
     answer = Answer.query.filter_by(is_gold_standard=True, location_id=location_id).first()
     return answer
@@ -232,9 +263,9 @@ def remove_answer(answer_id):
     ------
     exception : Exception
         When no answer is found.
-
     """
     answer = get_answer_by_id(answer_id)
+
     if answer is None:
         raise Exception("Cannot find answer with id ", answer_id)
 
@@ -244,7 +275,7 @@ def remove_answer(answer_id):
 
 def is_answer_passed(answer_id):
     """
-    Test if the answer passes the test (with the gold standard)
+    Test if the answer passes the data quality check (with the gold standard)
 
     Parameters
     ----------
@@ -254,20 +285,22 @@ def is_answer_passed(answer_id):
     Raises
     ------
     exception : Exception
-        When no answer is found.    
-    """    
+        When no answer is found.
+    exception : Exception
+        When no corresponding gold standard answer is found.
+    """
     is_passed = False
     my_answer = get_answer_by_id(answer_id)
-    
-    if(my_answer is None):
+
+    if my_answer is None:
         raise Exception("Cannot find the answer.")
 
     gold_answer = Answer.query.filter_by(is_gold_standard=True, location_id=my_answer.location_id).first()
 
-    if(gold_answer is None):
+    if gold_answer is None:
         raise Exception("Cannot find the gold standard answer.")
 
-    if(gold_answer.land_usage == my_answer.land_usage and gold_answer.expansion == my_answer.expansion):
+    if gold_answer.land_usage == my_answer.land_usage and gold_answer.expansion == my_answer.expansion:
         is_passed = True
 
-    return is_passed    
+    return is_passed
