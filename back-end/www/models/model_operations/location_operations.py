f2c946de2825747d9c50d99446031ce432004724
YAlgorithm <legenddolphin.public@gmail.com>
2021-12-05 15:30:15 +0100

    Improve the documentation and format of the location_operations.py file.

--- back-end/www/models/model_operations/location_operations.py
+++ back-end/www/models/model_operations/location_operations.py
@@ -16,7 +16,8 @@ from models.model_operations.answer_operations import get_answers_by_user
 DEBUG = False
 def dbprint(*values: object):
     """
-    print for debugging purpose. Enabled by DEBUG flag.
+    The printing function for debugging.
+    Enabled by the DEBUG flag.
     """
     if DEBUG:
         print(values)
@@ -29,17 +30,12 @@ def create_location(factory_id):
     Parameters
     ----------
     factory_id : str
-        ID (uuid) provided by importing from the factory table
+        ID (uuid) provided by importing from the factory table.
 
     Returns
     -------
     location : Location
-        The newly created location.  
-
-    Raises
-    ------
-    exception : Exception
-        When photo year is not an integer        
+        The newly created location.
     """
     location = Location(factory_id=factory_id)
 
@@ -87,14 +83,14 @@ def get_location_by_factory_id(factory_id):
 
 def set_location_done(location_id, is_done):
     """
-    Set the current time to done_at to mark it's done.
+    Set the current time to done_at to mark it is done.
 
     Parameters
     ----------
     location_id : int
         ID of the location.
     is_done : bool
-        Set done or not done.   
+        Set done or not done.
 
     Returns
     -------
@@ -104,18 +100,19 @@ def set_location_done(location_id, is_done):
     Raises
     ------
     exception : Exception
-        -is_done is not bool
-        -When no location is found.
+        When is_done is not bool
+    exception : Exception
+        When no location is found.
     """
-    if(not isinstance(is_done, bool)):
+    if not isinstance(is_done, bool):
         raise Exception("is_done shall be bool")
 
     location = Location.query.filter_by(id=location_id).first()
 
     if location is None:
         raise Exception("No location found in the database to update.")
-    
-    if(is_done):
+
+    if is_done:
        location.done_at = datetime.datetime.now()
     else:
        location.done_at = None
@@ -149,7 +146,7 @@ def remove_location(location_id):
 
 def get_locations(user_client_id, size, gold_standard_size):
     """
-    Get #size-#gold_standard_size locations(exclusive of the user answered ones) plus #gold_standard_size locations which have gold standards.
+    Get the locations that can be returned to the front-end.
 
     Parameters
     ----------
@@ -158,7 +155,8 @@ def get_locations(user_client_id, size, gold_standard_size):
     size : int
         Total number of locations to be returned.
     gold_standard_size : int
-        Within size, the number of locations which includes gold standard answers        
+        The number of locations that should include gold standard answers.
+        There should be ("size" - "gold_standard_size") locations that are not labeled yet.
 
     Returns
     -------
@@ -167,25 +165,29 @@ def get_locations(user_client_id, size, gold_standard_size):
 
     Raises
     ------
-    exception : Exception when either
-        - size and gold_standard are not integers, or < 0
-        - gold standard doesn't exist
-        - gold standard size exceeds size
-        - Cannot find #gold_standard_size of locations which have gold standards
-        - Cannot find #size of locations
+    exception : Exception
+        When size and gold_standard_size are not integers (or < 0).
+    exception : Exception
+        When gold standard does not exist.
+    exception : Exception
+        When gold standard size exceeds size.
+    exception : Exception
+        When we cannot find "gold_standard_size" of locations which have gold standards.
+    exception : Exception
+        When we cannot find "size" of locations.
     """
-    if(not isinstance(gold_standard_size, int)):
+    if not isinstance(gold_standard_size, int):
         raise Exception("The gold_standard_size shall be an integer")
-    if(not isinstance(size, int)):
+    if not isinstance(size, int):
         raise Exception("The gold_standard_size shall be an integer")
-    if(size < 0):
+    if size < 0:
         raise Exception("The size must be greater or equal to 0.")
-    if(gold_standard_size < 0):
+    if gold_standard_size < 0:
         raise Exception("The gold_standard_size must be greater or equal to 0.")
     if gold_standard_size > size:
         raise Exception("The gold standard size cannot exceed size.")
 
-    if(size==0): 
+    if size == 0:
         return None
 
     # Get the user's answers
@@ -193,25 +195,26 @@ def get_locations(user_client_id, size, gold_standard_size):
     user_answers = get_answers_by_user(target_user.id)
     user_answered_location_id_list = [answer.location_id for answer in user_answers]
 
-    #Get the locations which have gold answers
-    gold_answers_filter = Answer.query.filter(Answer.is_gold_standard)    
+    # Get the locations which have gold answers
+    gold_answers_filter = Answer.query.filter(Answer.is_gold_standard)
     gold_location_id_list = [loc.location_id for loc in gold_answers_filter.distinct(Answer.location_id).all()]
 
-    if(gold_location_id_list is None):
+    if gold_location_id_list is None:
         raise Exception("No gold standards exist. DB not initialized?", size)
 
-    if(len(gold_location_id_list) < gold_standard_size):
-        err_rstring = "Cannot find expected(enough) amount of locations which have gold standards :{}. {} are found.".format(gold_standard_size, len(gold_location_list))
-        raise Exception(err_rstring)
+    if len(gold_location_id_list) < gold_standard_size:
+        err_msg = "Cannot find expected amount of locations which have gold standards :{}. {} are found.".format(
+                gold_standard_size, len(gold_location_list))
+        raise Exception(err_msg)
 
-    # Get the locations which have been answered by the user    
+    # Get the locations which have been answered by the user
     user_identified_location_filter = Location.query.filter(Location.id.in_(user_answered_location_id_list))
     user_identified_locations = [loc.id for loc in user_identified_location_filter.all()]
 
     gold_location_filter = Location.query.filter(Location.id.in_(gold_location_id_list))
-    
-    # get to-be-excluded location ids, which are not either with gold answers nor identified by the user before
-    if(user_identified_locations is not None):
+
+    # Get to-be-excluded location ids, which are not either with gold answers or identified by the user before
+    if user_identified_locations is not None:
         exclude_location_id_list = gold_location_id_list + user_identified_locations
     else:
         exclude_location_id_list = gold_location_id_list
@@ -221,15 +224,14 @@ def get_locations(user_client_id, size, gold_standard_size):
     sel_gold_location_list = None
     sel_non_gold_location_list = None
 
-    # randomly sort and select the first locations which has been provideded gold answers
+    # Randomly sort and select the first locations which has been provideded gold answers
     if gold_standard_size > 0:
         rand_gold_location_list = gold_location_filter.order_by(func.random()).all()
         dbprint("Got {} gold rand_gold_location_list.".format(len(rand_gold_location_list)))
-
         sel_gold_location_list = rand_gold_location_list[0:gold_standard_size]
         dbprint("sel_gold_location_list : ", sel_gold_location_list)
 
-    # randomly sort and select the first locations which has exclude user answered ones plus those with gold answers
+    # Randomly sort and select the first locations which has exclude user answered ones plus those with gold answers
     if size > gold_standard_size:
         rand_none_gold_location_list = non_gold_locations_filter.order_by(func.random()).all()
         sel_non_gold_location_list = rand_none_gold_location_list[0:(size - gold_standard_size)]
@@ -237,12 +239,12 @@ def get_locations(user_client_id, size, gold_standard_size):
 
     # Combine and shuffle the list
     location_list = sel_non_gold_location_list
-    if(sel_gold_location_list is not None):
+    if sel_gold_location_list is not None:
         location_list += sel_gold_location_list
         random.shuffle(location_list)
 
     # Double confirm the amount
-    if(len(location_list) < size):
+    if len(location_list) < size:
         raise Exception("Cannot find expected amount of locations", size)
 
     return location_list
@@ -250,11 +252,12 @@ def get_locations(user_client_id, size, gold_standard_size):
 
 def get_location_is_done_count():
     """
-    Get the count of locations which have been labled done
+    Get the count of locations which have been labled done.
 
     Returns
     -------
-    count : the amount of locations which have been labled done
+    count : int
+        The amount of locations which have been labled done.
     """
     # Create an exclusive filter to get locations which have done_at date
     location_query = Location.query.filter(Location.done_at.isnot(None))
@@ -264,11 +267,12 @@ def get_location_is_done_count():
 
 def get_location_count():
     """
-    Get the total number of locations in the db
+    Get the total number of locations.
 
     Returns
     -------
-    count : the total number of locations
+    count : int
+        The total number of locations.
     """
     count = Location.query.count()
     return count
