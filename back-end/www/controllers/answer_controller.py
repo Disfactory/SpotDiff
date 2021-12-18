a89f7795f0ec02352f33e0851d765849bf465b6d
Sourbiebie <sourbiebie@gmail.com>
2021-12-18 21:43:37 +0800

    Move batch_process_answers to model operations.
    Call batch_process_answers to batch submit answers.

--- back-end/www/controllers/answer_controller.py
+++ back-end/www/controllers/answer_controller.py
@@ -7,8 +7,7 @@ from util.util import InvalidUsage
 from util.util import handle_invalid_usage
 from util.util import decode_user_token
 from config.config import config
-from models.model_operations.answer_operations import create_answer
-from models.model_operations.answer_operations import check_answer_correctness
+from models.model_operations.location_operations import batch_process_answers
 
 
 bp = Blueprint("answer_controller", __name__)
@@ -19,8 +18,15 @@ def answer():
     The function for the front-end to submit answers if passing the gold-standard test.
 
     Sample command to test:
-    $curl -d '{"user_token":"xxx", "data":[{"location_id":"123", "year_new":2017, "year_old":2010, "source_url_root":"www.test.org", "left_top_lat":24.0962704615941, "left_top_lng":120.462878886353,"bottom_right_lat":24.0962704615941, "bottom_right_lng":120.462878886353, "land_usage":1, "expansion":0, "zoom_level":0}]}' -H "Content-Type: application/json" -X POST http://localhost:5000/answer/
 
+    $curl -d '{"user_token":"xx","data":[\
+    {"location_id":1, "year_new":2017, "year_old":2010, "source_url_root":\
+    "www.test.org", "is_gold_standard":false, "bbox_left_top_lat":0, "bbox_left_top_lng":0,\
+    "bbox_bottom_right_lat":0, "bbox_bottom_right_lng":0, "land_usage":1, "expansion":, "zoom_level":0}, \
+    {"location_id":8, "year_new":2017, "year_old":2010, "source_url_root":"www.test.org", \
+    "is_gold_standard":false, "bbox_left_top_lat":0, "bbox_left_top_lng":0,"bbox_bottom_right_lat":0,\
+     "bbox_bottom_right_lng":0, "land_usage":1, "expansion":1, "zoom_level":0}]}' -H "Content-Type: application/json"\
+     -X POST http://localhost:5000/answer/
 
     Parameters
     ----------
@@ -95,53 +101,18 @@ def answer():
             e = InvalidUsage("Please provide correct user token.", status_code=400)
             return handle_invalid_usage(e) 
 
-
         if "data" not in rj:
             e = InvalidUsage("Please provide data.", status_code=400)
             return handle_invalid_usage(e) 
 
         # Check all the answers from frontend to decide the next step.
-        pass_status = False
-        submit_id_list = []
-        for idx in range(len(rj["data"])):
-            if "location_id" not in rj["data"][idx] or \
-                "land_usage" not in rj["data"][idx] or \
-                "expansion" not in rj["data"][idx]:
+        pass_status = batch_process_answers(user_id, rj["data"])
 
-                e = InvalidUsage("Data must have location id, land_usage, and expansion.", status_code=400)
-                return handle_invalid_usage(e)
+        if pass_status:
+            return_status = {"Passed" : True}
+        else:            
+            return_status = {"Passed" : False}
 
-            # Check every answer if gold standard exists
-            result = check_answer_correctness(rj["data"][idx]["location_id"], rj["data"][idx]["land_usage"], rj["data"][idx]["expansion"])
-            
-            # If anyone failed the gold standard test, return Failed immediately.
-            if result == 1:
-                return_status = {"Passed" : False}
-                return jsonify(return_status)
-
-            # Gold standard doesn't exist. Append the id for answer submission.
-            elif result == 2:
-                submit_id_list.append(idx)
-                pass_status = True
-            # If the answer passes the gold standard test, mark pass_status true to see if we can continue the submit.                    
-            else:    
-                pass_status = True
-
-        # Quality check doesn't pass or cannot identified.
-        if pass_status == False:
-            e = InvalidUsage("Please provide valid answers.", status_code=400)
-            return handle_invalid_usage(e)
-
-        # Submit the answers. Always submit with is_gold_standard = False.
-        for idx in submit_id_list:
-            create_answer(user_id, rj["data"][idx]["location_id"], 
-                         rj["data"][idx]["year_old"], rj["data"][idx]["year_new"], rj["data"][idx]["source_url_root"], 
-                         rj["data"][idx]["land_usage"], rj["data"][idx]["expansion"], False,
-                         rj["data"][idx]["bbox_left_top_lat"], rj["data"][idx]["bbox_left_top_lng"], 
-                         rj["data"][idx]["bbox_bottom_right_lat"], rj["data"][idx]["bbox_bottom_right_lng"], rj["data"][idx]["zoom_level"], 
-                         )
-
-        return_status = {"Passed" : True}
         return jsonify(return_status)
 
 
