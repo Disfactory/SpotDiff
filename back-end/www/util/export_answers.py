1745ca837b89c2b12226680cf3a88f45e1a88f3d
Sourbiebie <sourbiebie@gmail.com>
2022-02-12 11:17:41 +0800

    Add client_id and factory_id
    Dump more "readable" data for testers.

--- back-end/www/util/export_answers.py
+++ back-end/www/util/export_answers.py
@@ -18,6 +18,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import csv
 from models.model import db
 from models.model import Answer
+from models.model_operations import location_operations
+from models.model_operations import answer_operations
+from models.model_operations import user_operations
 from config.config import Config
 from flask import Flask
 from controllers import root
@@ -33,17 +36,22 @@ app.app_context().push()
 cvs_file_name = "answer_" + datetime.datetime.today().strftime("%Y_%m_%d_%H_%M_%S") + ".csv"
 print("Exporting answers to " + cvs_file_name + "...")
 
+# Get all answers
 answer_query = Answer.query.order_by(Answer.user_id)
 answers = answer_query.all()
 
 with open(cvs_file_name, "w", newline="") as csvDataFile:
-
+    # Write header
     csvWriter = csv.writer(csvDataFile, delimiter=",", quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvWriter.writerow(["Userid", "location_id", "answer_id", "land_usage", "expansion", "gold_standard_status", 
+    csvWriter.writerow(["Userid", "client_id", "location_id", "factory_id", "answer_id", "land_usage", "expansion", "gold_standard_status", 
                         "bbox_left_top_lat", "bbox_left_top_lng", "bbox_bottom_right_lat", "bbox_bottom_right_lng", 
                         "zoom_level", "timestamp"])
     for answer in answers:
-        csvWriter.writerow([answer.user_id, answer.location_id, answer.id, answer.land_usage,answer.expansion, 
+        # Write each record in answer table
+        factory_id = location_operations.get_location_by_id(answer.location_id).factory_id
+        client_id = user_operations.get_user_by_id(answer.user_id).client_id
+
+        csvWriter.writerow([answer.user_id, client_id, answer.location_id, factory_id, answer.id, answer.land_usage,answer.expansion, 
         answer.gold_standard_status, answer.bbox_left_top_lat, answer.bbox_left_top_lng, answer.bbox_bottom_right_lat, 
         answer.bbox_bottom_right_lng, answer.zoom_level, answer.timestamp])
 
