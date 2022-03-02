19ae551b4c4b641bc11ad36e526c533767185acf
YAlgorithm <legenddolphin.public@gmail.com>
2022-03-02 14:42:52 +0100

    Add one requirement.

--- back-end/install_packages.sh
+++ back-end/install_packages.sh
@@ -8,6 +8,7 @@ pip install --upgrade flask-marshmallow==0.14.0
 pip install --upgrade marshmallow-sqlalchemy==0.24.2
 pip install --upgrade flask-cors==3.0.10
 pip install --upgrade marshmallow_enum==1.5.1
+pip install --upgrade itsdangerous==2.0.1
 
 # Testing
 pip install --upgrade flask-testing==0.8.1
