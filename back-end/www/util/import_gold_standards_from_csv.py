d7d2c307464031599b7635192d70c6a533eda847
Sourbiebie <sourbiebie@gmail.com>
2022-04-19 23:52:24 +0800

    add test code and utility to delete gold answers
    https://github.com/Disfactory/SpotDiff/issues/30

--- back-end/www/util/import_gold_standards_from_csv.py
+++ back-end/www/util/import_gold_standards_from_csv.py
@@ -11,8 +11,14 @@ Output
 The total location numbers after import.
 
 """
-CSV_FILE_NAME = "50_answer_gold_standard_delete.csv"
+CSV_FILE_NAME = "answer_gold_standard_update20220419.csv"
+#CSV_FILE_NAME = "50_answer_gold_standard.csv"
+
+# For Staging server
 CFG_NAME = "config.config.DevelopmentConfig"
+
+# For production server
+#CFG_NAME = "config.config.ProductionConfig" 
 
 import sys
 import os
@@ -54,22 +60,29 @@ with open(CSV_FILE_NAME) as csvDataFile:
     next(csvReader)
     
     # for every row, insert the id(row 1) into the location table
+    
     for row in csvReader:
         if row[0] is None:
             break
         location = location_operations.get_location_by_factory_id(row[0])
-        if location is not None:
-            print("location_id is: {}".format(location))
-            gold_answer = answer_operations.get_gold_answer_by_location(location)
-            if gold_answer is not None:
-                answer_operations.set_answer_gold_standard_status(gold_answer, row[5])
-                print("Update gold_standard_status of answer of location {}".format(row[0]))
-                update_count = update_count + 1
-            else:         
+        if location is None:
+            location = location_operations.create_location(row[0])
+            print("Create location : {}".format(location.id))
+
+        gold_answer = answer_operations.get_gold_answer_by_location(location.id)
+
+        # If the gold answer exists, and the new gold_standard_status is specified
+        if gold_answer is not None and len(row) >5 and row[5] is not None:
+            answer_operations.set_answer_gold_standard_status(gold_answer.id, int(row[5]))
+            print("Update gold_standard_status of answer of location {}".format(row[0]))
+            update_count = update_count + 1
+        elif gold_answer is None:
+            # If the gold standard doesn't exist, and the file specify a gold standard, or in old format to create one
+            if (len(row) > 5 and int(row[5]) == 0) or (len(row) <= 5):
                 answer = answer_operations.create_answer(u1.id, location.id, int(row[2]), int(row[1]), "", int(row[3]), int(row[4]), 0)
+                print("create answer")
                 ans_count = ans_count + 1
-        else:
-            print("Cannot insert {}".format(row[0]))
+        
 
 print("Updated {} gold standards.".format(update_count))
 print("Insert {} gold standards. ".format(ans_count))
